57036aecbf939d8f7e872e48d8a655ec93536897
shawshanks <root@DESKTOP-SF9PNUD.localdomain>
2019-07-13 23:46:27 +0800

    Quick Start finish

--- app.py
+++ app.py
@@ -1,17 +1,50 @@
-# ----Flask Hello World ---#
+# ---- Flask Hello World ---- #
 
 # import the Flask class from the flask package
+
 from flask import Flask
 
 # create the application object
 app = Flask(__name__)
 
-# user the decorator pattern to link the view function to a url
+# use the decorator pattern to
+# link the view function to a url
 @app.route("/")
 @app.route("/hello")
 # define the view using a function, which returns a string
 def hello_world():
     return "Hello, World!"
+
+
+@app.route("/test/<search_query>")
+def search(search_query):
+    return search_query
+
+
+@app.route("/integer/<int:value>")
+def int_type(value):
+    print(value + 1)
+    return "correct"
+
+
+@app.route("/float/<float:value>")
+def float_type(value):
+    print(value + 1)
+    return "correct"
+
+
+@app.route("/path/<path:value>")
+def path_type(value):
+    print(value)
+    return "correct"
+
+
+@app.route("/name/<name>")
+def index(name):
+    if name.lower() == "michael":
+        return f"Hello, {name}"
+    else:
+        return "Not Found", 404
 
 
 # start the development server using the run() method
